f4176b41b11f4a60c12a8f91dc3f0391f51b62a2
sodaus145 <shinseongjune.dev@gmail.com>
2021-08-05 13:38:39 +0900

    innergamemanager

---
+++ Assets/Scripts/Inner/InnerGameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InnerGameManager
+{
+    //singleton=========================================================
+    private static readonly InnerGameManager instance = new InnerGameManager();
+
+    static InnerGameManager()
+    {
+
+    }
+
+    private InnerGameManager()
+    {
+
+    }
+
+    public static InnerGameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    //===================================================================
+
+    public int playerTeam;
+}
